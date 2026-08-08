10f3bf77043e1f99694ddc04e20bc6e5c0ea0fe0
Josias De Oliveira <61791505+josiasoliveira12@users.noreply.github.com>
2026-08-08 08:22:01 -0300

    Add files via upload

---
+++ Atividade 4/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Array {
+    static void lerVetor(int[] vetor) {
+        Console.WriteLine();
+        Console.WriteLine("Entre com os dados do vetor: ");
+        for(int i = 0; i < vetor.Length; i++) {
+            Console.Write($"Vetor na posiçao {i}: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+    }
+    static void gerarVetor(int[] vetor) {
+        Random aleatorio = new Random();
+        for(int i = 0; i < vetor.Length; i++)
+            vetor[i] = aleatorio.Next(1, 100);
+        
+    }
+    static void mostrarVetor(int [] vetor) {
+        Console.WriteLine();
+        Console.WriteLine("Dados do Vetor: ");
+        for(int i = 0; i < vetor.Length; i++) {
+            Console.Write(vetor[i] + " | ");
+        }
+    }
+    static void Main() {
+        int[] vetor = new int[5];
+        gerarVetor(vetor);
+        mostrarVetor(vetor);
+    }
+}
+
---
+++ Atividade 4/Exercicio_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Exercicio_1 {
+     static void GerarVetor(int[] vetor) {
+        Random aleatorio = new Random();
+        for(int i = 0; i < vetor.Length; i++)
+            vetor[i] = aleatorio.Next(1, 100);
+        
+    }
+     static void MostrarVetor(int[] vetor) {
+        Console.WriteLine();
+        Console.WriteLine("Dados do Vetor: ");
+        for(int i = 0; i < vetor.Length; i++) {
+            Console.Write(vetor[i] + " | ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+    static int SomarVetor(int[] vetor) {
+        int soma = 0;
+        for(int i = 0; i < vetor.Length; i++) {
+            soma += vetor[i];
+        }
+        return soma;
+    }
+    static void Main() {
+        int n, resultadoSoma;
+        Console.WriteLine("Qual o tamanho do vetor: ");
+        n = int.Parse(Console.ReadLine());
+        int[] vetor = new int[n];
+        GerarVetor(vetor);
+        MostrarVetor(vetor);
+        resultadoSoma = SomarVetor(vetor);
+        Console.WriteLine($"Soma dos elementos do vetor: {resultadoSoma}");
+        
+    }
+    
+}
+
